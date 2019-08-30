204372d31af920102dd96644b1ddb66b94c4966d
freitzzz <1160907@isep.ipp.pt>
2019-08-30 19:29:09 +0100

    Implemented Error() function so FieldError implements error interface
    see #3

--- src/model/fielderror.go
+++ src/model/fielderror.go
@@ -1,5 +1,7 @@
 package model
 
+import "fmt"
+
 // FieldError is a custom error for invalid fields found during
 // the initialization of a model
 type FieldError struct {
@@ -7,4 +9,8 @@ type FieldError struct {
 	Field string
 	// and this string should be the name of the model which the error was created
 	Model string
+}
+
+func (e *FieldError) Error() string {
+	return fmt.Sprintf("Field: %s on Model: %s is invalid", e.Field, e.Model)
 }
