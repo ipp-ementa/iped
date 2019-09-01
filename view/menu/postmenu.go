46c3584b6e3b01a29fbd8fb395560ff1b6c16cd4
freitzzz <1160907@isep.ipp.pt>
2019-09-01 18:03:24 +0100

    Added gofile containing structs that represent
    the model views for the POST functionalities of menus collection

---
+++ view/menu/postmenu.go
@@ -0,0 +1,12 @@
+package menu
+
+// CreateNewMenuModelView is the model view representation
+// for the create new menu functionality
+// See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/menus.md#create-a-new-menu
+type CreateNewMenuModelView struct {
+	Type   string `json:"type"`
+	Dishes []struct {
+		Type        string `json:"type"`
+		Description string `json:"description"`
+	} `json:"dishes"`
+}
