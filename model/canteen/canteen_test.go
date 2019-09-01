f01049a535a33111232525802cf54b949412a050
freitzzz <1160907@isep.ipp.pt>
2019-09-01 12:18:51 +0100

    Added unit tests for AvailableMenus method

--- model/canteen/canteen_test.go
+++ model/canteen/canteen_test.go
@@ -3,6 +3,9 @@ package canteen
 import (
 	"testing"
 
+	"github.com/ipp-ementa/iped/model/dish"
+	"github.com/ipp-ementa/iped/model/menu"
+
 	"github.com/ipp-ementa/iped/model/customerror"
 )
 
@@ -35,5 +38,46 @@ func TestNotEmptyCanteenNameDoesNotReturnError(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Canteen initilization should have been successful but got %s", err)
+	}
+}
+
+func TestAvailableMenusMethodReturnsEmptySliceIfNoMenusAreAvailable(t *testing.T) {
+	_canteen, _ := New("Cantina do H")
+
+	availableMenus := _canteen.AvailableMenus()
+
+	if lenab := len(availableMenus); lenab != 0 {
+		t.Errorf("The length of availableMenus slice should be 0 but got: %d", lenab)
+	}
+}
+
+func TestAvailableMenusMethodReturnsUnmodifiableSlice(t *testing.T) {
+	_canteen, _ := New("Cantina do H")
+
+	availableMenus := _canteen.AvailableMenus()
+
+	// This verification is to grant that the returned available menus slice length is 0
+
+	if lenab := len(availableMenus); lenab != 0 {
+		t.Errorf("The length of availableMenus slice should be 0 but got: %d", lenab)
+	}
+
+	// If we add a new menu to the the returned slice,
+	// it should not modify the slice pointed on the canteen struct
+
+	_dish, _ := dish.New(0, "Fried Noodles")
+
+	_menu, _ := menu.New(0, []dish.Dish{_dish})
+
+	availableMenus = append(availableMenus, _menu)
+
+	if lenam := len(availableMenus); lenam != 1 {
+		t.Errorf("The length of availableMenus slice should now be 1 but got: %d", lenam)
+	}
+
+	availableMenusAfterModification := _canteen.AvailableMenus()
+
+	if lenaam := len(availableMenusAfterModification); lenaam != 0 {
+		t.Errorf("The length of availableMenusAfterModification slice should be 0 but got: %d", lenaam)
 	}
 }
