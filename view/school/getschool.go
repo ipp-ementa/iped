82e65cf101f7e7551905c26f7b9bf44d5727866b
freitzzz <1160907@isep.ipp.pt>
2019-09-02 17:26:23 +0100

    Added function that creates a GetAvailableSchoolsModelView using a slice of schools

--- view/school/getschool.go
+++ view/school/getschool.go
@@ -1,5 +1,7 @@
 package school
 
+import "github.com/ipp-ementa/iped/model/school"
+
 // This file contains model views representation for GET functionalities of schools collection
 
 // GetAvailableSchoolsModelView is the model view representation
@@ -15,11 +17,27 @@ type GetAvailableSchoolsModelView []struct {
 // for the detailed school information functionality
 // See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/schools.md#detailed-school-information
 type GetDetailedSchoolInformationModelView struct {
-	ID       int    `json:"id"`
-	Acronym  string `json:"acronym"`
-	Name     string `json:"name"`
-	Canteens []struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"canteens"`
+	ID       int                   `json:"id"`
+	Acronym  string                `json:"acronym"`
+	Name     string                `json:"name"`
+	Canteens []innerCanteensStruct `json:"canteens"`
+}
+
+type innerCanteensStruct struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// ToGetAvailableSchoolsModelView creates a GetAvailableSchoolsModelView using a slice of school models
+func ToGetAvailableSchoolsModelView(schools []school.School) GetAvailableSchoolsModelView {
+	modelview := make(GetAvailableSchoolsModelView, len(schools))
+
+	for index, school := range schools {
+		element := &modelview[index]
+		element.ID = int(school.ID)
+		element.Acronym = school.Acronym
+		element.Name = school.Name
+	}
+
+	return modelview
 }
