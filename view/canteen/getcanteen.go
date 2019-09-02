95a4dd44de32254792939a7f724dcdd7b8c62afc
freitzzz <1160907@isep.ipp.pt>
2019-09-02 17:16:32 +0100

    Added function that creates a GetAvailableCanteensModelView using a slice of canteen models

--- view/canteen/getcanteen.go
+++ view/canteen/getcanteen.go
@@ -1,5 +1,7 @@
 package canteen
 
+import "github.com/ipp-ementa/iped/model/canteen"
+
 // This file contains model views representation for GET functionalities of canteens collection
 
 // GetAvailableCanteensModelView is the model view representation
@@ -16,4 +18,17 @@ type GetAvailableCanteensModelView []struct {
 type GetDetailedCanteenInformationModelView struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+// ToGetAvailableCanteensModelView creates a GetAvailableCanteensModelView using a slice of canteen models
+func ToGetAvailableCanteensModelView(canteens []canteen.Canteen) GetAvailableCanteensModelView {
+	modelview := make(GetAvailableCanteensModelView, len(canteens))
+
+	for index, canteen := range canteens {
+		element := &modelview[index]
+		element.ID = int(canteen.ID)
+		element.Name = canteen.Name
+	}
+
+	return modelview
 }
