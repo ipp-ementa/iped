3ee7d8a58dec00ebec853cd377c403ccaf709248
freitzzz <1160907@isep.ipp.pt>
2019-09-02 20:43:52 +0100

    Implemented controller logic for detailed school information functionality

--- controller/school/api.go
+++ controller/school/api.go
@@ -2,6 +2,7 @@ package school
 
 import (
 	"net/http"
+	"strconv"
 
 	model "github.com/ipp-ementa/iped/model/school"
 	view "github.com/ipp-ementa/iped/view/school"
@@ -24,13 +25,45 @@ func AvailableSchools(c echo.Context) error {
 
 	// Finds all available schools
 
-	db.Find(&schools)
+	err := db.Find(&schools).Error
 
-	if len(schools) == 0 {
+	if err != nil || len(schools) == 0 {
 		return c.JSON(http.StatusNotFound, nil)
 	}
 
 	modelview := view.ToGetAvailableSchoolsModelView(schools)
+
+	return c.JSON(http.StatusOK, modelview)
+
+}
+
+// DetailedSchoolInformation handles GET /schools/:id functionality
+// See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/schools.md#available-schools
+func DetailedSchoolInformation(c echo.Context) error {
+
+	db, ok := c.Get("db").(*gorm.DB)
+
+	if !ok {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	id, perr := strconv.Atoi(c.Param("id"))
+
+	if perr != nil {
+		return c.JSON(http.StatusNotFound, nil)
+	}
+
+	var school model.School
+
+	// Finds school by ID
+
+	err := db.Find(&school, id).Error
+
+	if err != nil {
+		return c.JSON(http.StatusNotFound, nil)
+	}
+
+	modelview := view.ToGetDetailedSchoolInformationModelView(school)
 
 	return c.JSON(http.StatusOK, modelview)
 
