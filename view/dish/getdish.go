4e4e7d318417da742a4cea09fdd9e94828be8f33
freitzzz <1160907@isep.ipp.pt>
2019-09-02 15:52:23 +0100

    Added function that creates a GetAvailableDishesModelView using a slice of dishes

--- view/dish/getdish.go
+++ view/dish/getdish.go
@@ -1,5 +1,7 @@
 package dish
 
+import "github.com/ipp-ementa/iped/model/dish"
+
 // This file contains model views representation for GET functionalities of dishes collection
 
 // GetAvailableDishesModelView is the model view representation
@@ -18,4 +20,18 @@ type GetDetailedDishInformationModelView struct {
 	ID          int    `json:"id"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
+}
+
+// DishesToGetAvailableDishesModelView creates a GetAvailableDishesModelView using a slice of dishes
+func DishesToGetAvailableDishesModelView(dishes []dish.Dish) GetAvailableDishesModelView {
+	modelview := make(GetAvailableDishesModelView, len(dishes))
+
+	for index, dish := range dishes {
+		element := modelview[index]
+		element.ID = int(dish.ID)
+		element.Description = dish.Description
+		element.Type = dish.Type.String()
+	}
+
+	return modelview
 }
