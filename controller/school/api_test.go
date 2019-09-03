369c387880676f431920554df4bd58b662513ab1
freitzzz <1160907@isep.ipp.pt>
2019-09-03 20:08:06 +0100

    Added unit tests for DetailedSchoolInformation functionality

--- controller/school/api_test.go
+++ controller/school/api_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/ipp-ementa/iped/model/canteen"
@@ -69,11 +70,87 @@ func TestAvailableSchoolsReturnsOKIfSchoolsExist(t *testing.T) {
 	ctx.SetPath("/schools")
 	ctx.Set("db", db.Db)
 
-	// GET /schools
+	// GET /schools/:id
 	AvailableSchools(ctx)
 
 	if rec.Code != http.StatusOK {
 		t.Errorf("There is one school available in the database so the response status code should be 200 but was: %d", rec.Code)
 	}
 
+}
+
+func TestDetailedSchoolInformationReturnsInternalServerErrorIfDatabaseIsNotAvailable(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools/:id")
+	ctx.SetParamNames("id")
+	ctx.SetParamValues("1")
+
+	// GET /schools
+	DetailedSchoolInformation(ctx)
+
+	if rec.Code != http.StatusInternalServerError {
+		t.Errorf("No middleware was setup to provide the database connection object to the request, so the response status code should be 500 but was: %d", rec.Code)
+	}
+}
+
+func TestDetailedSchoolInformationReturnsNotFoundIfResourceIdIsNotAnIntegerParsable(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools/:id")
+	ctx.SetParamNames("id")
+	ctx.SetParamValues("non integer")
+	ctx.Set("db", db.Db)
+
+	// GET /schools/:id
+	DetailedSchoolInformation(ctx)
+
+	if rec.Code != http.StatusNotFound {
+		t.Errorf("The resource id is not a parsable intenger so the response status code should be 404 but was: %d", rec.Code)
+	}
+}
+
+func TestDetailedSchoolInformationReturnsNotFoundIfResourceWasNotFound(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools/:id")
+	ctx.SetParamNames("id")
+	ctx.SetParamValues("0")
+	ctx.Set("db", db.Db)
+
+	// GET /schools/:id
+	DetailedSchoolInformation(ctx)
+
+	if rec.Code != http.StatusNotFound {
+		t.Errorf("There is no school with the resource id '0' so the response status code should be 404 but was: %d", rec.Code)
+	}
+}
+
+func TestDetailedSchoolInformationReturnsOKIfResourceWasFound(t *testing.T) {
+
+	_canteen, _ := canteen.New("Cantina do ESMAE")
+
+	_school, _ := school.New("ESMAE", "Escola Superior de Música e Artes do Espetáculo", []canteen.Canteen{_canteen})
+
+	db.Db.Create(&_school)
+
+	id := _school.ID
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools/:id")
+	ctx.SetParamNames("id")
+	ctx.SetParamValues(strconv.FormatUint(uint64(id), 10))
+	ctx.Set("db", db.Db)
+
+	// GET /schools/:id
+	DetailedSchoolInformation(ctx)
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("There is a school with the resource id %d so the response status code should be 200 but was: %d", id, rec.Code)
+	}
 }
