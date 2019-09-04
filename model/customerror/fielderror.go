5b29fe0f7f7c7aaead22f93b15c7b9a3c61b5cfe
freitzzz <1160907@isep.ipp.pt>
2019-09-04 15:36:24 +0100

    Added new property to FieldError model which describes the cause
    of the error

--- model/customerror/fielderror.go
+++ model/customerror/fielderror.go
@@ -7,10 +7,12 @@ import "fmt"
 type FieldError struct {
 	// this string should be the name of the field which is invalid
 	Field string
-	// and this string should be the name of the model which holds the invalid field
+	// this string should be the name of the model which holds the invalid field
 	Model string
+	// and this string should be the explanation of the error
+	Explanation string
 }
 
 func (e *FieldError) Error() string {
-	return fmt.Sprintf("Field: %s on Model: %s is invalid", e.Field, e.Model)
+	return fmt.Sprintf("Field: %s on Model: %s is invalid due to: %s", e.Field, e.Model, e.Explanation)
 }
