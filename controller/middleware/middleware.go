efc031b01cb5c3afe87d378c8ae60b0e1247ecfe
freitzzz <1160907@isep.ipp.pt>
2019-09-05 14:58:49 +0100

    Added middleware function that verifies if a resource identifier
    is a valid integer or if its greater than zero
    If not, it responds with 404 Not Found

--- controller/middleware/middleware.go
+++ controller/middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
 import (
+	"net/http"
+	"strconv"
+	"strings"
+
 	"github.com/ipp-ementa/iped/controller/db"
 	"github.com/labstack/echo"
 )
@@ -11,6 +15,26 @@ func DbAccessMiddleware() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			c.Set("db", db.Db)
 			next(c)
+			return nil
+		}
+	}
+}
+
+// ResourceIdentifierValidationMiddleware is a middleware that checks if resource identifiers passed as params (eg: :id)
+// is a valid integer or if its greater than zero
+// If not it automatically responds with 404 Not Found
+func ResourceIdentifierValidationMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			params := c.ParamNames()
+			for _, param := range params {
+				if strings.Contains(param, "id") {
+					id, err := strconv.Atoi(param)
+					if err != nil || id <= 0 {
+						return c.NoContent(http.StatusNotFound)
+					}
+				}
+			}
 			return nil
 		}
 	}
