a03972e9302cc286f2eabc823895ec9a2efacaa5
freitzzz <1160907@isep.ipp.pt>
2019-09-05 14:28:01 +0100

    Added CreateNewSchool unit tests

--- controller/school/api_test.go
+++ controller/school/api_test.go
@@ -5,10 +5,12 @@ import (
 	"net/http/httptest"
 	"os"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/ipp-ementa/iped/model/canteen"
 	"github.com/ipp-ementa/iped/model/school"
+	model "github.com/ipp-ementa/iped/model/school"
 
 	"github.com/ipp-ementa/iped/controller/db"
 
@@ -58,7 +60,7 @@ func TestAvailableSchoolsReturnsOKIfSchoolsExist(t *testing.T) {
 
 	_canteen, _ := canteen.New("Cantina do H")
 
-	_school, _ := school.New("ISEP", "Instituto Superior de Engenharia do Porto", []canteen.Canteen{_canteen})
+	_school, _ := model.New("ISEP", "Instituto Superior de Engenharia do Porto", []canteen.Canteen{_canteen})
 
 	// Inserts a school in the database
 
@@ -152,5 +154,144 @@ func TestDetailedSchoolInformationReturnsOKIfResourceWasFound(t *testing.T) {
 
 	if rec.Code != http.StatusOK {
 		t.Errorf("There is a school with the resource id %d so the response status code should be 200 but was: %d", id, rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsInternalServerErrorIfDatabaseIsNotAvailable(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusInternalServerError {
+		t.Errorf("No middleware was setup to provide the database connection object to the request, so the response status code should be 500 but was: %d", rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsBadRequestIfRequestBodyIsEmpty(t *testing.T) {
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+	ctx.Set("db", db.Db)
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusBadRequest {
+		t.Errorf("The request body is empty so the response status code should be 400 but was : %d", rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsBadRequestIfCanteenFieldsAreInvalid(t *testing.T) {
+
+	json := `{
+		"acronym":"ISEP", 
+		"name":"Instituto Superior de Engenharia do Porto", 
+		"canteens":[
+			{
+				"name":""
+			}	
+		]
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(json))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+	ctx.Set("db", db.Db)
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusBadRequest {
+		t.Errorf("The request body an invalid canteen so the response status code should be 400 but was : %d", rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsBadRequestIfSchoolFieldsAreInvalid(t *testing.T) {
+	json := `{
+		"acronym":"ISEP", 
+		"name":"Instituto Superior de Engenharia do Porto"`
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(json))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+	ctx.Set("db", db.Db)
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusBadRequest {
+		t.Errorf("The request body provides no canteens so the response status code should be 400 but was : %d", rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsBadRequestIfSchoolWithSameAcronymAlreadyExists(t *testing.T) {
+
+	_canteen, _ := canteen.New("Cantina da ESS")
+
+	_canteens := []canteen.Canteen{_canteen}
+
+	_school, _ := model.New("ESS", "Escola Superior de Saúde", _canteens)
+
+	db.Db.Create(&_school)
+
+	json := `{
+		"acronym":"ESS", 
+		"name":"Escola Superior de Saúde", 
+		"canteens":[
+			{
+				"name":"Cantina da ESS"
+			}	
+		]
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(json))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+	ctx.Set("db", db.Db)
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusBadRequest {
+		t.Errorf("The school being created already exists so the response status code should be 400 but was : %d", rec.Code)
+	}
+}
+
+func TestCreateNewSchoolReturnsCreatedIfAllSchoolFieldsAreValid(t *testing.T) {
+	json := `{
+		"acronym":"IPP", 
+		"name":"Instituto Politécnico do Porto", 
+		"canteens":[
+			{
+				"name":"Cantina do IPP"
+			}	
+		]
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(json))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := ech.NewContext(req, rec)
+	ctx.SetPath("/schools")
+	ctx.Set("db", db.Db)
+
+	// POST /schools
+	CreateNewSchool(ctx)
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("All school fields are valid so the response status should be created: %d", rec.Code)
 	}
 }
