a2be9c03c41937931b3d430d854a8f96cb7d8fed
freitzzz <1160907@isep.ipp.pt>
2019-09-05 03:30:03 +0100

    Added handler for Create New School functionality

--- controller/school/api.go
+++ controller/school/api.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 	"strconv"
 
+	customerrormodel "github.com/ipp-ementa/iped/model/customerror"
+	customerrorview "github.com/ipp-ementa/iped/view/customerror"
+
+	"github.com/ipp-ementa/iped/model/canteen"
+
 	model "github.com/ipp-ementa/iped/model/school"
 	view "github.com/ipp-ementa/iped/view/school"
 
@@ -18,7 +23,7 @@ func AvailableSchools(c echo.Context) error {
 	db, ok := c.Get("db").(*gorm.DB)
 
 	if !ok {
-		return c.JSON(http.StatusInternalServerError, nil)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	schools := []model.School{}
@@ -28,7 +33,7 @@ func AvailableSchools(c echo.Context) error {
 	err := db.Find(&schools).Error
 
 	if err != nil || len(schools) == 0 {
-		return c.JSON(http.StatusNotFound, nil)
+		return c.NoContent(http.StatusNotFound)
 	}
 
 	modelview := view.ToGetAvailableSchoolsModelView(schools)
@@ -38,19 +43,19 @@ func AvailableSchools(c echo.Context) error {
 }
 
 // DetailedSchoolInformation handles GET /schools/:id functionality
-// See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/schools.md#available-schools
+// See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/schools.md#detailed-school-information
 func DetailedSchoolInformation(c echo.Context) error {
 
 	db, ok := c.Get("db").(*gorm.DB)
 
 	if !ok {
-		return c.JSON(http.StatusInternalServerError, nil)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	id, perr := strconv.Atoi(c.Param("id"))
 
 	if perr != nil {
-		return c.JSON(http.StatusNotFound, nil)
+		return c.NoContent(http.StatusNotFound)
 	}
 
 	var school model.School
@@ -60,11 +65,79 @@ func DetailedSchoolInformation(c echo.Context) error {
 	err := db.Find(&school, id).Error
 
 	if err != nil {
-		return c.JSON(http.StatusNotFound, nil)
+		return c.NoContent(http.StatusNotFound)
 	}
+
+	// Find school canteens
+
+	db.Model(&school).Related(&school.CanteensSlice)
 
 	modelview := view.ToGetDetailedSchoolInformationModelView(school)
 
 	return c.JSON(http.StatusOK, modelview)
+
+}
+
+// CreateNewSchool handles POST /schools functionality
+// See more info at: https://github.com/ipp-ementa/iped-documentation/blob/master/documentation/rest_api/schools.md#create-a-new-school
+func CreateNewSchool(c echo.Context) error {
+
+	db, ok := c.Get("db").(*gorm.DB)
+
+	if !ok {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	var modelview view.CreateNewSchoolModelView
+
+	c.Bind(&modelview)
+
+	canteens := make([]canteen.Canteen, len(modelview.Canteens))
+
+	for index := range modelview.Canteens {
+		canteen, cerr := canteen.New(modelview.Canteens[index].Name)
+		if cerr != nil {
+
+			modelview := customerrorview.UsingFieldErrorToErrorMessageModelView(*cerr)
+
+			return c.JSON(http.StatusBadRequest, modelview)
+		}
+		canteens[index] = canteen
+	}
+
+	school, serr := model.New(modelview.Acronym, modelview.Name, canteens)
+
+	if serr != nil {
+
+		modelview := customerrorview.UsingFieldErrorToErrorMessageModelView(*serr)
+
+		return c.JSON(http.StatusBadRequest, modelview)
+	}
+
+	var existingSchool model.School
+
+	// Finds if school with same acronym already exists
+
+	err := db.Where(map[string]interface{}{"acronym": modelview.Acronym}).First(&existingSchool).Error
+
+	if err == nil {
+
+		cerr := customerrormodel.FieldError{Field: "acronym", Model: "school", Explanation: "a school with the same acronym already exists"}
+
+		modelview := customerrorview.UsingFieldErrorToErrorMessageModelView(cerr)
+
+		return c.JSON(http.StatusBadRequest, modelview)
+	}
+
+	// Creates school
+	cerr := db.Create(&school).Error
+
+	if cerr != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	modelviewres := view.ToGetDetailedSchoolInformationModelView(school)
+
+	return c.JSON(http.StatusCreated, modelviewres)
 
 }
