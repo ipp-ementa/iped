aad4f46ba94a1b428d7a7f00c9218b6016601e58
freitzzz <1160907@isep.ipp.pt>
2019-09-07 16:35:48 +0100

    Added instructions to launch IPED

--- main.go
+++ main.go
@@ -1,4 +1,65 @@
 // This gofile is the entrypoint for iped
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/ipp-ementa/iped/controller/canteen"
+	"github.com/ipp-ementa/iped/controller/db"
+	"github.com/ipp-ementa/iped/controller/dish"
+	"github.com/ipp-ementa/iped/controller/menu"
+	"github.com/ipp-ementa/iped/controller/middleware"
+	"github.com/ipp-ementa/iped/controller/school"
+	"github.com/labstack/echo"
+)
+
+func main() {
+
+	ech := echo.New()
+
+	ech.Use(middleware.DbAccessMiddleware())
+
+	ech.Use(middleware.ResourceIdentifierValidationMiddleware())
+
+	// schools collection functionalities
+
+	ech.GET("/schools", school.AvailableSchools)
+
+	ech.GET("/schools/:id", school.DetailedSchoolInformation)
+
+	ech.POST("/schools", school.CreateNewSchool)
+
+	// canteens collection functionalities
+
+	ech.GET("/schools/:id/canteens", canteen.AvailableCanteens)
+
+	ech.GET("/schools/:id/canteens/:id2", canteen.DetailedCanteenInformation)
+
+	ech.POST("/schools/:id/canteens", canteen.CreateNewCanteen)
+
+	// menus collection functionalities
+
+	ech.GET("/schools/:id/canteens/:id2/menus", menu.AvailableMenus)
+
+	ech.GET("/schools/:id/canteens/:id2/menus/:id3", menu.DetailedMenuInformation)
+
+	ech.POST("/schools/:id/canteens/:id2/menus", menu.CreateNewMenu)
+
+	// dishes collection functionalities
+
+	ech.GET("/schools/:id/canteens/:id2/menus/:id3/dishes", dish.AvailableDishes)
+
+	ech.GET("/schools/:id/canteens/:id2/menus/:id3/dishes/:id4", dish.DetailedDishInformation)
+
+	port, perr := strconv.Atoi(os.Getenv("IPED_PORT"))
+
+	if perr != nil {
+		panic(fmt.Sprint("Server couldn't be open as the specified port is not valid"))
+	}
+
+	ech.Start(fmt.Sprintf("localhost:%d", port))
+
+	defer db.Db.Close()
+}
