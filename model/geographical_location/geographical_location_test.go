a1a2c6bf1a1c4320b8e78cc0258bbaa22e1c73f7
freitzzz <1160907@isep.ipp.pt>
2020-02-15 19:18:06 +0000

    Added geographical location package tests see #15

---
+++ model/geographical_location/geographical_location_test.go
@@ -0,0 +1,73 @@
+package canteen
+
+import (
+	"testing"
+)
+
+func TestLatitudeWithAValueThatIsLowerThanMinimumBoundaryReturnError(t *testing.T) {
+
+	latitude := float32(-180.01)
+
+	longitude := float32(45.0)
+
+	_, err := New(latitude, longitude)
+
+	if err == nil {
+		t.Error("Geographical Location struct initilization should have returned an error but got nil")
+	}
+
+	if err.Field != "latitude" {
+		t.Error("Even though that geographical location struct initialization returned an error, the error should have been caused by the field latitude")
+	}
+}
+
+func TestLatitudeWithAValueThatIsHigherThanMaximumBoundaryReturnError(t *testing.T) {
+
+	latitude := float32(180.01)
+
+	longitude := float32(45.0)
+
+	_, err := New(latitude, longitude)
+
+	if err == nil {
+		t.Error("Geographical Location struct initilization should have returned an error but got nil")
+	}
+
+	if err.Field != "latitude" {
+		t.Error("Even though that geographical location struct initialization returned an error, the error should have been caused by the field latitude")
+	}
+}
+
+func TestLongitudeWithAValueThatIsLowerThanMinimumBoundaryReturnError(t *testing.T) {
+
+	latitude := float32(45.0)
+
+	longitude := float32(-90.01)
+
+	_, err := New(latitude, longitude)
+
+	if err == nil {
+		t.Error("Geographical Location struct initilization should have returned an error but got nil")
+	}
+
+	if err.Field != "longitude" {
+		t.Error("Even though that geographical location struct initialization returned an error, the error should have been caused by the field longitude")
+	}
+}
+
+func TestLongitudeWithAValueThatIsHigherThanMaximumBoundaryReturnError(t *testing.T) {
+
+	latitude := float32(45)
+
+	longitude := float32(180.01)
+
+	_, err := New(latitude, longitude)
+
+	if err == nil {
+		t.Error("Geographical Location struct initilization should have returned an error but got nil")
+	}
+
+	if err.Field != "longitude" {
+		t.Error("Even though that geographical location struct initialization returned an error, the error should have been caused by the field longitude")
+	}
+}
