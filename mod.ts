0c4485ba97be78162370f507634ff7d4c966c895
freitzzz <1160907@isep.ipp.pt>
2020-08-08 15:41:41 +0100

    Usage of routers

--- mod.ts
+++ mod.ts
@@ -1,7 +1,20 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+router.get("/sample", (ctx) => {
+  ctx.response.body = "sample";
+});
+
+router.get("/sample/:id", (ctx) => {
+  ctx.response.body = `sample with id: ${ctx.params.id}`;
+});
 
 const app = new Application();
 
+app.use(router.routes());
+
+app.use(router.allowedMethods());
 
 app.use((ctx) => {
   ctx.response.body = "Hello world!";
