5c789a738c27b3737b4e11c0e4a3b59c3bccaf8d
freitzzz <1160907@isep.ipp.pt>
2020-08-08 18:53:17 +0100

    Model canteen

---
+++ models/menu.ts
@@ -0,0 +1,30 @@
+import { Dish } from "./dish.ts";
+import { HasDuplicates, Ok, Result, Err } from "./common.ts";
+
+class Menu {
+  dishes: Dish[];
+
+  type: MenuType;
+
+  constructor(dishes: Dish[], type: MenuType) {
+    this.dishes = dishes;
+    this.type = type;
+  }
+
+  static create(dishes: Dish[], type: MenuType): Result<Menu, string> {
+    if (dishes.length == 0) {
+      return Err("must provide at least one dish");
+    } else if (HasDuplicates<Dish>(dishes)) {
+      return Err("cannot have duplicate dish");
+    } else {
+      return Ok(new Menu(dishes, type));
+    }
+  }
+}
+
+enum MenuType {
+  lunch,
+  dinner,
+}
+
+export { Menu, MenuType };
