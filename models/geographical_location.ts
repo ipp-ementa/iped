c9eade8fbfb3a5455963f89d0bf59025dcf4e961
freitzzz <1160907@isep.ipp.pt>
2020-08-08 18:53:43 +0100

    Model Geographical Location

---
+++ models/geographical_location.ts
@@ -0,0 +1,28 @@
+import { Err, Ok, Result } from "./common.ts";
+
+class GeographicalLocation {
+  latitude: number;
+
+  longitude: number;
+
+  constructor(latitude: number, longitude: number) {
+    this.latitude = latitude;
+
+    this.longitude = longitude;
+  }
+
+  static create(
+    latitude: number,
+    longitude: number,
+  ): Result<GeographicalLocation, string> {
+    if (latitude > 90 || latitude < -90) {
+      return Err("latitude must range [-90, 90]");
+    } else if (longitude > 90 || longitude < -90) {
+      return Err("longitude must range [-180, 180]");
+    } else {
+      return Ok(new GeographicalLocation(latitude, longitude));
+    }
+  }
+}
+
+export { GeographicalLocation };
