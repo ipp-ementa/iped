c610778ad6ff4eae748bb7fb01789e50276c49aa
freitzzz <1160907@isep.ipp.pt>
2020-08-10 23:49:44 +0100

    Expose more monads and mongo code on deps.ts

--- deps.ts
+++ deps.ts
@@ -1,5 +1,15 @@
 export {
   Err,
+  None,
   Ok,
+  Option,
   Result,
+  Some,
 } from "https://deno.land/x/monads@v0.3.4/mod.ts";
+
+export {
+  MongoClient,
+  ObjectId,
+  Collection,
+  QuerySelector,
+} from "https://deno.land/x/mongo@v0.9.2/mod.ts";
