9b042f0398d2d0856774724a4a040ee33588e26d
freitzzz <1160907@isep.ipp.pt>
2020-08-11 13:08:44 +0100

    typeFromString introduction

--- models/dish.ts
+++ models/dish.ts
@@ -1,22 +1,42 @@
-import { NonEmptyString, Equatable } from "./common.ts";
+import { NonEmptyString, Equatable, Result, Ok, Err } from "./common.ts";
 
 class Dish implements Equatable {
-  description: NonEmptyString;
+  description: string;
 
   type: DishType;
 
   private constructor(description: NonEmptyString, type: DishType) {
-    this.description = description;
+    this.description = description.valueOf();
     this.type = type;
   }
 
   equals(obj: any): boolean {
     return obj && obj.description === this.description;
   }
+
+  public static create(description: NonEmptyString, type: DishType): Dish {
+    return new Dish(description, type);
+  }
+
+  public static typeFromString(type: string): Result<DishType, string> {
+    switch (type) {
+      case DishType[0]:
+        return Ok(DishType.meat);
+      case DishType[1]:
+        return Ok(DishType.diet);
+      case DishType[2]:
+        return Ok(DishType.fish);
+      case DishType[3]:
+        return Ok(DishType.vegetarian);
+      default:
+        return Err(`type: ${type} is not valid`);
+    }
+  }
 }
 
 enum DishType {
   meat,
+  diet,
   fish,
   vegetarian,
 }
